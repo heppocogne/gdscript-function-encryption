64a8f63bedf0bb6e9ffcbd876cb8e8a210239040
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-07-05 18:07:09 +0900

    Update function_impl.gd

--- addons/function_encryption/function_impl.gd
+++ addons/function_encryption/function_impl.gd
@@ -2,7 +2,6 @@
 class_name FunctionImpl
 extends Resource
 
-var _gdscript:=GDScript.new()
 var _obj:=RefCounted.new()
 @export var data:Array[PackedByteArray]:
 	set(array):
@@ -16,9 +15,10 @@ var _obj:=RefCounted.new()
 		key.load("res://secret/id_rsa.key",false)
 		for bytes in encrypted:
 			decrypted.append_array(crypto.decrypt(key,bytes))
-		_gdscript.source_code=decrypted.get_string_from_utf8()
-		_gdscript.reload()
-		_obj.set_script(_gdscript)
+		var gdscript:=GDScript.new()
+		gdscript.source_code=decrypted.get_string_from_utf8()
+		gdscript.reload()
+		_obj.set_script(gdscript)
 
 
 func call_impl(method:StringName,this:Object=null,args=null):
